2404b20125a7b780e352d582181fc19018de5772
wajid9752 <68110483+wajid9752@users.noreply.github.com>
2023-02-15 11:32:49 +0530

    removed comment from models

--- src/models.py
+++ src/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-
 class Account(models.Model):
     user_id = models.CharField(max_length=20)
     name = models.CharField(max_length=20)
